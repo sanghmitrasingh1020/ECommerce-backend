0c420747c3439a1a8645a9cd06e5559169f2327f
Sanghmitra Singh <singhsanghmitra9@gmail.com>
2023-08-27 23:12:34 +0530

    test

--- index.js
+++ index.js
@@ -22,11 +22,12 @@ const ordersRouter = require('./routes/Order');
 const { User } = require('./model/User');
 const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
 const path = require('path');
+const { Order } = require('./model/Order');
 
 // Webhook
 // TODO: we will capture actual order after deploying out server live on public URL
 const endpointSecret = process.env.ENDPOINT_SECRET;
-server.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
+server.post('/webhook', express.raw({ type: 'application/json' }), async (request, response) => {
     const sig = request.headers['stripe-signature'];
     let event;
     try {
@@ -39,7 +40,9 @@ server.post('/webhook', express.raw({ type: 'application/json' }), (request, res
     switch (event.type) {
         case 'payment_intent.succeeded':
             const paymentIntentSucceeded = event.data.object;
-            // Then define and call a function to handle the event payment_intent.succeeded
+            const order = await Order.findById(paymentIntentSucceeded.metadata.orderId);
+            order.paymentStatus = 'received';
+            await order.save();
             break;
         // ... handle other event types
         default:
