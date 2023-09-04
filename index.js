6cf92de156f48869308d555b4abdac19b12507e5
Sanghmitra Singh <singhsanghmitra9@gmail.com>
2023-09-04 12:11:26 +0530

    test1.2

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ server.post('/webhook', express.raw({ type: 'application/json' }), async (reques
     switch (event.type) {
         case 'payment_intent.succeeded':
             const paymentIntentSucceeded = event.data.object;
-
+            console.log({paymentIntentSucceeded})
             const order = await Order.findById(paymentIntentSucceeded.metadata.orderId);
             order.paymentStatus = 'received';
             await order.save()
